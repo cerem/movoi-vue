ea0be00fbc4af5edfcd32836710b9972e1f08069
Cerem Beyazıt <cerembeyazit@gmail.com>
2017-07-21 18:07:51 +0300

    Arama fonksiyonu, Arayüz değişimi, - hala beta.

--- js/app.js
+++ js/app.js
@@ -4,11 +4,31 @@ new Vue({
     logotype: '[m]',
     movie_name: '',
     statusText: '',
-    remoteSearchJSONData: ''
+    remoteSearchJSONData: []
   },
   methods: {
     movie_search: function () {
-      
+
+      // OMDB API'si
+      var omdbAPIKey = 'cfcb4f93';
+
+      // OMDb API URL'si
+      var omdbURL = 'https://www.omdbapi.com/?s=' + this.movie_name + '&apikey=' + omdbAPIKey;
+
+      // axios.js ile veri çekelim
+
+      // Vue'nin kendisini unutmayalım
+      var self = this;
+
+      axios.get(omdbURL)
+        .then(function (response) {
+          // Gelen veri içindeki Search kısmındaki objeleri Vue'nin değerine atayalım.
+          self.remoteSearchJSONData = [];
+          self.remoteSearchJSONData = response.data.Search;
+        })
+        .catch(function (error) {
+          self.statusText = "Bir hata oluştu. " + error
+        });
     }
   }
 })
